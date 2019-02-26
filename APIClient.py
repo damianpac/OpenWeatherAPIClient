36cd1d92d469e48a94108ca4a8d32c4a0bd7f0f1
damianpac <damian.pacholec@engineer.com>
2019-02-27 00:36:17 +0100

    Update APIClient.py

--- APIClient.py
+++ APIClient.py
@@ -3,12 +3,12 @@ from pprint import pprint
 
 def main():
     city = 'Warsaw'
-    response = requests.get('http://api.openweathermap.org/data/2.5/weather?q=' + city +'&appid=e4164bc6c9e7305bfecf321bfd6222b1&units=metric')
+    response = requests.get('http://api.openweathermap.org/data/2.5/weather?q=' + city +'&appid=DEFAULTAPPID&units=metric')
     weather = response.json()
     #pprint(weather)
     print('The weather for: ', weather['name'],' (', weather['coord']['lon'], ', ', weather['coord']['lat'], ')')
     print(weather['main']['temp'])
-    print(weather['weather'][0]['description']) # zero, bo to 1 item na liście, która jest słownikiem
+    print(weather['weather'][0]['description'])
 
 if __name__ == '__main__':
     main()
